98d6d978ebb5833d00069215a3372e29ad292077
tobman4 <36448579+tobman4@users.noreply.github.com>
2024-06-12 01:44:14 +0200

    update dbg-dump.py

--- dbg-dump.py
+++ dbg-dump.py
@@ -1,8 +1,58 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
+import json
 
-from lol import summoner
+import rich.json
+from rich.panel import Panel
+from rich import pretty, console
+
+from lol import summoner,lockfile, lobby,client, eog
+
+parser = ArgumentParser("TODO")
+
+parser.add_argument(
+  "-l",
+  default="C:\\Riot Games\\League of Legends\\lockfile",
+  dest="lockfile",
+  type=FileType("r")
+)
+
+args = parser.parse_args()
+
+def render_data_block(name: str, data: any):
+  if(data is None):
+    c.print(f"[bold red]No {name} data![/bold red]")
+    return
+
+  json_str = json.dumps(data)
+  p = Panel(
+    rich.json.JSON(
+      json_str,
+      indent=2
+    )
+  )
+  p.title = name
+  c.print(p)
+
+  pass
 
 if __name__ == "__main__":
+  
+  # Prep
+  data = {}
+  # pretty.install()
+  c = console.Console()
 
-  user = summoner.get_summoner()
-  print(user)
+  lockfile.load_file(args.lockfile)
+
+  # Collet data
+  with c.status("Getting data...") as status:
+    user = summoner.get_summoner()
+    party = lobby.get_lobby()  
+    gamephase = client.get_gameflow_phase()
+    eog_data = eog.get_eog_data()
+
+
+  render_data_block("User", user)
+  render_data_block("Lobby", party)
+  render_data_block("EOG", eog_data)
+  c.print(f"[bold]Phase:[/bold] {gamephase}")
