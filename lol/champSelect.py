8422b91f5a3db282825fed2cf6e28c0968020eaf
tobman4 <36448579+tobman4@users.noreply.github.com>
2024-06-14 13:22:10 +0200

    test

---
+++ lol/champSelect.py
@@ -0,0 +1,83 @@
+import json
+
+import requests
+
+import lockfile
+
+def shallow_call(func):
+  def wrapper():
+    result = func()
+
+    if(not result.ok):
+      return None
+  
+    return result.json()
+
+  return wrapper
+
+@shallow_call
+def set_session():
+  url = lockfile.get_url("lol-champ-select/v1/session")
+
+  return requests.get(
+    url=url,
+    verify=False,
+    auth=lockfile.get_auth()
+  )
+
+def get_current_action():
+  session = set_session()
+
+  if(session is None):
+    return
+
+  inProgress = []
+
+  actions = session.get("actions")
+
+  for action_group in actions:
+    for action in action_group:
+      if(action["isInProgress"]):
+        inProgress.append(action)
+
+  return inProgress
+
+  pass
+
+def complete_actions(data):
+  action_id = data["id"]
+
+  path_url = lockfile.get_url(f"lol-champ-select/v1/session/actions/{action_id}")
+  comlete_url = lockfile.get_url(f"lol-champ-select/v1/session/actions/{action_id}/complete")
+
+  # Update action
+  requests.patch(
+    url=path_url,
+    verify=False,
+    auth=lockfile.get_auth(),
+    headers={
+      "Content-Type": "application/json"
+    },
+    data=json.dumps(data)
+  )
+
+  # Complete
+  requests.post(
+    url=comlete_url,
+    verify=False,
+    auth=lockfile.get_auth()
+  )
+
+# TODO: Get local player
+
+if __name__ == "__main__":
+  lockfile.load_path("C:\Riot Games\League of Legends\lockfile")
+
+
+  p = get_current_action()
+  a = p[0]
+
+  if(p["type"] == "ban"):
+    a["championId"] = 103
+
+  complete_actions(a)
