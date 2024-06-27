638ec0b0d32b3d2821d02fd7e1cece529be976bb
tobman4 <36448579+tobman4@users.noreply.github.com>
2024-06-27 23:53:10 +0200

    use cool

--- lol/summoner.py
+++ lol/summoner.py
@@ -5,11 +5,7 @@ from . import lockfile
 def get_summoner():
   url = f"https://127.0.0.1:{lockfile.get_port()}/lol-summoner/v1/current-summoner"
 
-  result = requests.get(
-    url=url,
-    verify=False,
-    auth=("riot", lockfile.get_password())
-  )
+  result = lockfile.get_session().get(url=url)
 
   if(not result.ok):
     return None
